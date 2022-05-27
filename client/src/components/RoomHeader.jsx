974e23d9ec6663059481f95f8c80508c867a0465
MohanadOO <moohand220@gmail.com>
2022-05-27 03:05:05 +0300

    A Seperate Component for the Room Header

---
+++ client/src/components/RoomHeader.jsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react'
+import UsersDetails from './UsersDetails'
+import { RoomContext } from '../Context/RoomContext'
+
+function RoomHeader() {
+  const { room } = useContext(RoomContext)
+
+  return (
+    <div className='flex items-center rounded-lg gap-2 py-2 px-8 bg-primary-400'>
+      <img
+        className='w-8 rounded-full bg-white'
+        src={room.avatar}
+        alt='room_icon'
+      />
+      <p className='px-2 text-white'>{room.room}</p>
+      <div className='ml-auto'>
+        <UsersDetails />
+      </div>
+    </div>
+  )
+}
+
+export default RoomHeader
