1b67f9a7cb26d1bb30f6999335a8040de24d7c2a
MohanadOO <moohand220@gmail.com>
2022-05-27 03:06:09 +0300

    Add More Icons (Moon, Sun, Chaveron, Bell)

--- client/src/components/Icons.jsx
+++ client/src/components/Icons.jsx
@@ -95,3 +95,83 @@ export function AddIcon(props) {
     </svg>
   )
 }
+
+export function MoonIcon(props) {
+  return (
+    <svg
+      className='w-6 h-6'
+      fill='none'
+      stroke='currentColor'
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
+      />
+    </svg>
+  )
+}
+
+export function SunIcon(props) {
+  return (
+    <svg
+      className='w-6 h-6'
+      fill='none'
+      stroke='currentColor'
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        {...props}
+        d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
+      />
+    </svg>
+  )
+}
+
+export function ChevronUpIcon(props) {
+  return (
+    <svg
+      className='w-6 h-6'
+      fill='none'
+      stroke='currentColor'
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        d='M5 15l7-7 7 7'
+      />
+    </svg>
+  )
+}
+
+export function BellIcon(props) {
+  return (
+    <svg
+      className='w-6 h-6'
+      fill='none'
+      stroke='currentColor'
+      viewBox='0 0 24 24'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2}
+        d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'
+      />
+    </svg>
+  )
+}
