fdfeaf571c2adee016eddbe6232f60a39d365713
MohanadOO <moohand220@gmail.com>
2022-05-27 03:14:50 +0300

    🔄 Update ChatArea.jsx

--- client/src/components/ChatArea.jsx
+++ client/src/components/ChatArea.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import { useState, useEffect, useContext } from 'react'
 import { ChatContext } from '../Context/ChatContext'
 import { RoomContext } from '../Context/RoomContext'
+import RoomHeader from './RoomHeader'
 
 function ChatArea() {
   const { socket, currentUser } = useContext(ChatContext)
@@ -9,29 +9,32 @@ function ChatArea() {
   const [chat, setChat] = useState([])
   const [message, setMessage] = useState('')
 
-  const sendMessage = () => {
-    setChat((prevMessages) => {
-      return [
-        ...prevMessages,
-        {
-          message,
-          sender: currentUser.uid,
-          userProfile: currentUser.photoURL,
-        },
-      ]
-    })
-    socket.emit('send_message', {
-      chat: [
-        ...chat,
-        {
-          message,
-          sender: currentUser.uid,
-          userProfile: currentUser.photoURL,
-        },
-      ],
-      room,
-    })
-    setMessage('')
+  const sendMessage = (e) => {
+    e.preventDefault()
+    if (message !== '') {
+      setChat((prevMessages) => {
+        return [
+          ...prevMessages,
+          {
+            message,
+            sender: currentUser.uid,
+            userProfile: currentUser.photoURL,
+          },
+        ]
+      })
+      socket.emit('send_message', {
+        chat: [
+          ...chat,
+          {
+            message,
+            sender: currentUser.uid,
+            userProfile: currentUser.photoURL,
+          },
+        ],
+        room,
+      })
+      setMessage('')
+    }
   }
 
   useEffect(() => {
@@ -41,56 +44,72 @@ function ChatArea() {
   }, [socket])
 
   return (
-    <div className='bg-blue-200 flex-1'>
-      <p className='p-2 text-black'>{room}</p>
-      {chat.map((message, index) => {
-        return (
-          <div className='flex gap-5 items-center justify-center'>
-            {message.sender === currentUser.uid ? (
-              <>
-                <img
-                  className='rounded-full w-10'
-                  src={currentUser?.photoURL}
-                  alt='user_icon'
-                />
-                <p
-                  key={index}
-                  className='  bg-red-200 py-1 flex-1 my-2 px-5 text-xl rounded-md'
-                >
-                  {message.message}
-                </p>
-              </>
-            ) : (
-              <>
-                <p
-                  key={index}
-                  className='  bg-blue-200 py-1 my-2 flex-1 px-5 text-xl rounded-md'
-                >
-                  {message.message}
-                </p>
-                <img
-                  className='rounded-full w-10'
-                  src={`${message.userProfile}`}
-                  alt='user_icon'
-                />
-              </>
-            )}
-          </div>
-        )
-      })}
-      <input
-        className='p-2  '
-        type='text'
-        placeholder='Message...'
-        onChange={(e) => setMessage(e.target.value)}
-        value={message}
-      />
-      <button
-        className='bg-black text-primary-50 ml-5 my-5 p-2 rounded-md'
-        onClick={sendMessage}
-      >
-        Send Message
-      </button>
+    <div className='rounded-md flex-1 flex flex-col max-w-5xl justify-between relative shadow-md'>
+      <RoomHeader />
+      <div className='flex flex-col  justify-start overflow-y-auto'>
+        {chat.map((message, index) => {
+          return (
+            <div className='ml-5'>
+              {message.sender === currentUser.uid ? (
+                <div className='flex items-center gap-3'>
+                  <img
+                    className='rounded-md w-7 self-start'
+                    src={
+                      JSON.parse(localStorage.getItem('user')).userAvatar ||
+                      currentUser?.photoURL
+                    }
+                    alt='user_icon'
+                  />
+                  <div>
+                    <p className='text-black text-sm '>
+                      {JSON.parse(localStorage.getItem('user')).userName ||
+                        currentUser?.displayName}
+                    </p>
+                    <p
+                      key={index}
+                      className='bg-black/10 py-1 max-w-sm mb-7 px-3 text-md rounded-lg rounded-tl-none break-words'
+                    >
+                      {message.message}
+                    </p>
+                  </div>
+                </div>
+              ) : (
+                <div className='flex items-center gap-3 justify-end'>
+                  <div>
+                    <p className='text-black text-sm '>{message.userName}</p>
+                    <p
+                      key={index}
+                      className='bg-primary-400 text-white py-1 max-w-sm mb-4 px-3 text-md rounded-lg rounded-tr-none break-words'
+                    >
+                      {message.message}
+                    </p>
+                  </div>
+                  <img
+                    className='rounded-md w-7 self-start'
+                    src={message.userProfile}
+                    alt='user_icon'
+                  />
+                </div>
+              )}
+            </div>
+          )
+        })}
+        <form className='p-5 flex gap-2 items-center'>
+          <input
+            className='py-6 px-7 rounded-xl h-10 w-full bg-transparent shadow-none border-primary-400 border-2'
+            type='text'
+            placeholder='Write a Message...'
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+          <button
+            className='bg-primary-500 text-white w-40 py-3 px-2 rounded-md '
+            onClick={sendMessage}
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
