d8704e88738f23c5d728483a3a2443f60bbb9f4a
MohanadOO <moohand220@gmail.com>
2022-05-30 10:04:06 +0300

    Create Room Modal Component

--- client/src/components/SidePanel.jsx
+++ client/src/components/SidePanel.jsx
@@ -1,12 +1,12 @@
-import { MyModal } from './DialogModal'
-import { DiscloseRooms } from './DiscloseRooms'
-import { DiscloseUsers } from './DiscloseUsers'
+import { CreateRoomModal } from './Modals/CreateRoomModal'
+import { DiscloseRooms } from './Disclosure/DiscloseRooms'
+import { DiscloseUsers } from './Disclosure/DiscloseUsers'
 
 function SidePanel() {
   return (
     <div className=' w-64 overflow-y-auto scrollbar-thin scrollbar-thumb-primary-50 scrollbar-track-primary-50 hover:scrollbar-thumb-slate-500  px-2 '>
       <div className=' cursor-pointer my-4 px-7'>
-        <MyModal />
+        <CreateRoomModal />
         <DiscloseRooms />
         <DiscloseUsers />
       </div>
