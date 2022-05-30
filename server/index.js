9c49b46c41178c200cdca1a9bed1a4441a01f070
MohanadOO <moohand220@gmail.com>
2022-05-30 09:59:46 +0300

    Send Room ID

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ io.on('connection', (socket) => {
 
   socket.on('send_message', (data) => {
     console.log(data)
-    socket.to(data.room).emit('receive_message', data)
+    socket.to(data.room.id).emit('receive_message', data)
   })
 
   socket.on('disconnect', () => {
