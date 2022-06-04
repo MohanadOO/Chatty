249c455d0cd4b4f5508a2ddbe25d6eaeea9c55a0
MohanadOO <moohand220@gmail.com>
2022-06-04 23:35:44 +0300

    Get User Friends List.

--- client/src/routes/Chat.jsx
+++ client/src/routes/Chat.jsx
@@ -2,23 +2,26 @@ import { useState, useContext, useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 import { UserContext } from '../Context/UserContext'
 import { useAuth } from '../Firebase'
+import { getDoc } from 'firebase/firestore'
 import io from 'socket.io-client'
-import ChatInterface from '../components/ChatInterface'
-import CreateRoom from '../components/CreateRoom'
+import ChatContainer from '../components/ChatContainer'
 import { ChatContext } from '../Context/ChatContext'
-import { getUserRooms } from '../Firebase'
+import { getUserRooms, getFriends } from '../Firebase'
 import { StageSpinner } from 'react-spinners-kit'
 
 const socket = io.connect('http://localhost:3001')
 
 function Chat() {
+  //Current User Information
   const currentUser = useAuth()
   const [loading, setLoading] = useState(true)
   const [userRooms, setUserRooms] = useState(undefined)
+  const [userFriends, setUserFriends] = useState(undefined)
   const { userLoggedIn } = useContext(UserContext)
 
   useEffect(() => {
     if (currentUser?.displayName) {
+      //Get User Rooms from Database {name, id}
       getUserRooms(currentUser)
         .then((roomsArr) => {
           if (roomsArr) {
@@ -32,14 +35,33 @@ function Chat() {
         .catch((error) => {
           console.error(error)
         })
+
+      //Get User Friends from database.
+      getFriends(currentUser).then((i) => {
+        if (i) {
+          const friendDetailsPromiseArr = i.map(async (friend) => {
+            return await getDoc(friend.friendRef).then((info) => {
+              return { data: info.data(), id: info.id }
+            })
+          })
+          Promise.all(friendDetailsPromiseArr).then((item) => {
+            setUserFriends(item)
+          })
+        } else {
+          setUserFriends([])
+        }
+      })
     }
   }, [currentUser])
+
 
   return (
     <ChatContext.Provider
       value={{
         userRooms,
         setUserRooms,
+        userFriends,
+        setUserFriends,
         socket,
         currentUser,
       }}
@@ -51,7 +73,7 @@ function Chat() {
             <StageSpinner size={50} color={'#000'} />
           </div>
         ) : (
-          <>{!userRooms ? <CreateRoom /> : <ChatInterface />}</>
+          <ChatContainer />
         )}
       </>
     </ChatContext.Provider>
